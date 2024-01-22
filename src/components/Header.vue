4bd9ea6e5be9d0600c90bf3e11eb1b2d6c451bc7
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-22 19:00:19 -0300

    header component to search movies

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const emits = defineEmits(['onSearch'])
+    const query = ref('')
+
+    const handleEmit = () =>{
+        emits('onSearch', query.value)
+    }
+</script>
+
+<template>
+    <header class="w-full h-[75px] flex justify-between items-center p-4 bg-[#573b2a]">
+        <img 
+            src="../assets/moviemingle-high-resolution-logo-transparent.png" 
+            alt="Logo"
+            class="max-h-full max-w-[200px] object-contain"
+        >
+        <input 
+            type="text"
+            class="rounded-sm p-1"
+            placeholder="Search"
+            v-model="query"
+            @keypress.enter.prevent="handleEmit"
+        >
+    </header>
+</template>
