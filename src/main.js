fe15c4d436e5ce74e4e4fdb1c572395acb9275f0
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-22 17:05:38 -0300

    vue-awesome-paginate init config

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/router.js'
+import VueAwesomePaginate from "vue-awesome-paginate"
+import "vue-awesome-paginate/dist/style.css"
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueAwesomePaginate).mount('#app')
