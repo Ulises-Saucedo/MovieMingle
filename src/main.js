241c2b0819b779355bb8be2efa518a3e3d561b89
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-26 15:45:45 -0300

    vue icons added

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import App from './App.vue'
 import router from './router/router.js'
 import VueAwesomePaginate from "vue-awesome-paginate"
 import "vue-awesome-paginate/dist/style.css"
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { BiPlusCircle } from "oh-vue-icons/icons";
+addIcons(BiPlusCircle);
 
-createApp(App).use(router).use(VueAwesomePaginate).mount('#app')
+createApp(App).use(router).use(VueAwesomePaginate).component("v-icon", OhVueIcon).mount('#app')
