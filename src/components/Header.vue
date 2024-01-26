b8e0d6b26fdc3ab17f9e3f1b2fc93792e11f4bf8
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-26 16:12:38 -0300

    header emit changed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,29 @@
 <script setup>
-    import { ref } from 'vue'
+import { ref } from "vue";
 
-    const emits = defineEmits(['onSearch'])
-    const query = ref('')
+const emits = defineEmits(["onSearch"]);
+const query = ref("");
 
-    const handleEmit = () =>{
-        emits('onSearch', query.value)
-    }
+const handleEmit = () => {
+  emits("onSearch", query.value);
+};
 </script>
 
 <template>
-    <header class="w-full h-[75px] flex justify-between items-center p-4 bg-[#573b2a]">
-        <img 
-            src="../assets/moviemingle-high-resolution-logo-transparent.png" 
-            alt="Logo"
-            class="max-h-full max-w-[200px] object-contain"
-        >
-        <input 
-            type="text"
-            class="rounded-sm p-1"
-            placeholder="Search"
-            v-model="query"
-            @keypress.enter.prevent="handleEmit"
-        >
-    </header>
+  <header
+    class="w-full h-[75px] flex justify-between items-center p-4 bg-[#131315]"
+  >
+    <img
+      src="../assets/moviemingle-high-resolution-logo-transparent.png"
+      alt="Logo"
+      class="max-h-full max-w-[200px] object-contain"
+    />
+    <input
+      type="text"
+      class="rounded-sm p-1 bg-[#27272A] text-[#fff]"
+      placeholder="Search"
+      v-model="query"
+      @keypress.enter.prevent="handleEmit"
+    />
+  </header>
 </template>
