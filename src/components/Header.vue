50352e77e1fad7821bc2f4cf7a56c09e042c03cf
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-26 16:16:44 -0300

    img linked to main view

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,11 +13,13 @@ const handleEmit = () => {
   <header
     class="w-full h-[75px] flex justify-between items-center p-4 bg-[#131315]"
   >
-    <img
-      src="../assets/moviemingle-high-resolution-logo-transparent.png"
-      alt="Logo"
-      class="max-h-full max-w-[200px] object-contain"
-    />
+    <router-link to="/">
+      <img
+        src="../assets/moviemingle-high-resolution-logo-transparent.png"
+        alt="Logo"
+        class="max-h-full max-w-[200px] object-contain"
+      />
+    </router-link>
     <input
       type="text"
       class="rounded-sm p-1 bg-[#27272A] text-[#fff]"
