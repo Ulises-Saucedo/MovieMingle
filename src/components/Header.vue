0c55f29520437fd64dbb051094f418421721a1a9
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-26 18:10:52 -0300

    header emits updated

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { ref } from "vue";
+import { useRoute } from "vue-router";
 
-const emits = defineEmits(["onSearch"]);
+const route = useRoute();
+const emits = defineEmits(["onSearch", "onSearched"]);
 const query = ref("");
 
 const handleEmit = () => {
   emits("onSearch", query.value);
+};
+
+const handleSearch = () => {
+  emits("onSearched", query.value);
 };
 </script>
 
@@ -13,11 +19,11 @@ const handleEmit = () => {
   <header
     class="w-full h-[75px] flex justify-between items-center p-4 bg-[#131315]"
   >
-    <router-link to="/">
+    <router-link to="/" class="max-h-full max-w-[200px] flex justify-start">
       <img
         src="../assets/moviemingle-high-resolution-logo-transparent.png"
         alt="Logo"
-        class="max-h-full max-w-[200px] object-contain"
+        class="object-contain"
       />
     </router-link>
     <input
@@ -25,7 +31,9 @@ const handleEmit = () => {
       class="rounded-sm p-1 bg-[#27272A] text-[#fff]"
       placeholder="Search"
       v-model="query"
-      @keypress.enter.prevent="handleEmit"
+      @keypress.enter.prevent="
+        route.name == 'movies' ? handleEmit() : handleSearch()
+      "
     />
   </header>
 </template>
