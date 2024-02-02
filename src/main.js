94b5c8819360e7bf56deca7e7db226d86c5c3c7c
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-02 02:42:08 -0300

    some dependencies deleted

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/router.js'
-import VueAwesomePaginate from "vue-awesome-paginate"
-import "vue-awesome-paginate/dist/style.css"
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { BiPlusCircle } from "oh-vue-icons/icons";
 addIcons(BiPlusCircle);
