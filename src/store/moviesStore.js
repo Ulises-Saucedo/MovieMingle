1a81a109ba19b54d23e4092f4923b82ae6095097
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-04 18:00:39 -0300

    movies store | pinia added

---
+++ src/store/moviesStore.js
@@ -0,0 +1,38 @@
+import { defineStore } from "pinia";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const useMovies = defineStore("movies", {
+  state: () => ({
+    movies: [],
+    total_pages: 0,
+    current_page: 1,
+  }),
+  actions: {
+    async getTrendingMovies(page) {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/popular?page=${page}&api_key=${API_KEY}`
+        );
+        const { results } = await response.json();
+        this.movies = results;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async loadMoreData(url) {
+      this.current_page += 1;
+      if (this.current_page <= 500) {
+        try {
+          const response = await fetch(
+            `${url}page=${this.current_page}&api_key=${API_KEY}`
+          );
+          const { results } = await response.json();
+          this.movies.push(...results);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
+  },
+});
