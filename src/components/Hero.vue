c6ae2477c870452aa10f0c1ea10aaa055cc44ada
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-04 18:00:15 -0300

    hero component added

---
+++ src/components/Hero.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import TrendingSection from "./TrendingSection.vue";
+import { getUrl, getGenres } from "../helpers/moviesHelpers";
+import { useMovies } from "../store/moviesStore";
+
+const route = useRoute();
+const scrollComponent = ref(null);
+const store = useMovies();
+const genres = await getGenres();
+
+const handleScroll = async () => {
+  let element = scrollComponent.value;
+  const threshold = window.innerHeight + 100;
+  if (element.getBoundingClientRect().bottom <= threshold) {
+    await store.loadMoreData(getUrl(route.path));
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
+await store.getTrendingMovies(1);
+</script>
+
+<template>
+  <div
+    class="w-full h-[500px] relative bg-cover bg-top z-10"
+    :style="{
+      'background-image': `url(
+        'https://image.tmdb.org/t/p/original${store.movies[0].backdrop_path}'
+      )`,
+    }"
+  >
+    <div
+      class="bg-gradient-to-b from-transparent to-[#242335] w-full h-full absolute top-0 left-0 z-20"
+    ></div>
+    <div class="flex justify-end w-full p-5">
+      <input
+        type="text"
+        class="py-1 px-3 z-30 rounded-2xl font-normal bg-[#242335] text-[#fff] border-[1px] border-solid border-slate-600 focus:outline-1 focus:outline-double focus:outline-slate-500 placeholder:italic placeholder:text-slate-600"
+        placeholder="Search..."
+      />
+    </div>
+  </div>
+  <div ref="scrollComponent">
+    <TrendingSection :trending="store.movies" :genres="genres" />
+  </div>
+</template>
+
+<style scoped></style>
