20be85990d013e695b59987553cdc17799157cfa
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-05 18:00:45 -0300

    movie view updated

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,52 +1,50 @@
 <script setup>
 import { useRoute } from "vue-router";
-import MovieProvider from "../providers/movies.js";
+import { getMovie } from "../helpers/moviesHelpers";
 
 const route = useRoute();
-const movieRef = new MovieProvider();
-const movie = movieRef.movie;
-
-await movieRef.filterMovieById(route.params.id);
+const movie = await getMovie(route.params.id);
 </script>
 
 <template>
-  <main class="w-full h-screen justify-center items-center">
+  <div
+    class="w-full h-[500px] relative bg-cover bg-top z-10"
+    :style="{
+      'background-image': `url(
+        'https://image.tmdb.org/t/p/original${movie.backdrop_path}'
+      )`,
+    }"
+  >
     <div
-      class="flex flex-col md:flex-row max-h-[700px] max-w-[800px] p-4 mx-auto"
+      class="bg-gradient-to-b from-transparent to-[#242335] w-full h-full absolute top-0 left-0 z-20"
+    ></div>
+    <div
+      class="absolute z-30 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
     >
-      <section class="w-full md:w-1/2 h-full">
-        <img
-          :src="`https://image.tmdb.org/t/p/original/${movie.poster_path}`"
-          :alt="movie.title"
-          class="w-full h-full object-contain rounded"
-        />
-      </section>
-      <section class="flex flex-col ml-2 justify-between w-full md:w-1/2">
-        <section>
-          <div class="font-bold text-xl text-[#fff]">
-            {{ movie.title }}
-          </div>
-          <div class="mt-2 flex gap-2 text-[#fff]">
-            <div
-              v-for="genre in movie.genres"
-              class="inline p-1 rounded-sm bg-red-500"
-            >
-              {{ genre.name }}
-            </div>
-          </div>
-        </section>
-        <section>
-          <div class="text-[#fff]">
-            {{ movie.release_date }}
-          </div>
-          <div class="row text-[#fff]">
-            {{ movie.overview }}
-          </div>
-          <div class="w-full p-1 rounded bg-red-500 text-[#fff]">
-            <router-link to="/">Back</router-link>
-          </div>
-        </section>
-      </section>
+      <h1 class="text-5xl font-semibold text-[#fff]">{{ movie.title }}</h1>
     </div>
-  </main>
+  </div>
+  <div
+    class="flex flex-col gap-4 items-center max-h-[700px] max-w-[800px] p-4 mx-auto"
+  >
+    <div class="mt-2 flex gap-2 text-[#fff]">
+      <div
+        v-for="genre in movie.genres"
+        class="inline px-4 py-1 bg-red-500 rounded-2xl"
+      >
+        {{ genre.name }}
+      </div>
+    </div>
+    <div class="text-[#fff]">
+      {{ movie.release_date }}
+    </div>
+    <div class="row text-[#fff]">
+      {{ movie.overview }}
+    </div>
+    <router-link
+      to="/"
+      class="w-full h-full rounded-md py-1 bg-red-500 text-center text-[#fff]"
+      >Back</router-link
+    >
+  </div>
 </template>
