bcf669216db7c11a6c30b8cc09756335d8405e0d
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-02-05 18:25:44 -0300

    not found view added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center justify-center w-full h-screen p-5">
+    <h1 class="text-2xl text-[#fff]">Page not found, sorry 😔</h1>
+    <router-link
+      to="/"
+      class="w-full bg-red-500 max-w-[500px] rounded-md py-1 text-center text-[#fff] text-md mt-5"
+      >Go back</router-link
+    >
+  </div>
+</template>
